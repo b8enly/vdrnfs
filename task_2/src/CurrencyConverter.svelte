9f68597c91ee1a9248933e7481dbdf58e03d1b23
Yablonskaya Sofya <sofa512@yandex.ru>
2023-10-05 12:36:36 +0300

    task 2 v2

--- task_2/src/CurrencyConverter.svelte
+++ task_2/src/CurrencyConverter.svelte
@@ -1,39 +1,77 @@
 <script>
     import {onMount} from "svelte";
 
-    let usdAmount = 1;
+    let amount = 1;
+    let fromCurrency = "USD";
+    let toCurrency = "EUR";
+    let exchangeRates = {};
     let euroAmount = 0;
-    let exchangeRate = 0;
 
     onMount(async () => {
         try {
             const response = await fetch("https://open.er-api.com/v6/latest/USD");
             const data = await response.json();
-            exchangeRate = data.rates.EUR;
+            exchangeRates = data.rates;
             convert();
         } catch (error) {
-            console.error("Error fetching exchange rate:", error);
+            console.error("Ошибка при выполнении", error);
         }
     });
 
     function convert() {
-        euroAmount = (usdAmount * exchangeRate).toFixed(2);
+        const fromRate = exchangeRates[fromCurrency];
+        const toRate = exchangeRates[toCurrency];
+        const convertedAmount = (amount * (toRate / fromRate)).toFixed(2);
+        $: euroAmount = convertedAmount;
     }
 </script>
 
 <style>
-
+    .converter {
+        padding: 0 0 0 50px;
+    }
+    input {
+        margin-right: 15px;
+    }
+    select {
+        margin-right: 15px;
+    }
 </style>
 
 <main>
-    <label for="usdInput">USD:</label>
-    <input
-            type="number"
-            id="usdInput"
-            bind:value={usdAmount}
-            on:input={convert}
-    />
+    <div class="converter">
+        <h1>Конвертер валют</h1>
+        <label for="amountInput">Сумма: </label>
+        <input
+                type="number"
+                id="amountInput"
+                bind:value={amount}
+                on:input={convert}
+        />
 
-    <label for="euroInput">EUR:</label>
-    <input type="text" id="euroInput" bind:value={euroAmount} readonly/>
+        <label for="fromCurrencySelect">Валюта (для обмена):</label>
+        <select
+                id="fromCurrencySelect"
+                bind:value={fromCurrency}
+                on:change={convert}
+        >
+            {#each Object.keys(exchangeRates) as currency}
+                <option value={currency}>{currency}</option>
+            {/each}
+        </select>
+
+        <label for="toCurrencySelect">Валюта (получаемая):</label>
+        <select
+                id="toCurrencySelect"
+                bind:value={toCurrency}
+                on:change={convert}
+        >
+            {#each Object.keys(exchangeRates) as currency}
+                <option value={currency}>{currency}</option>
+            {/each}
+        </select>
+
+        <label for="result">Итоговая сумма: </label>
+        <input type="text" id="result" bind:value={euroAmount} readonly/>
+    </div>
 </main>
