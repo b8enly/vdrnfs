fc815a26bb998bd5ceb41ef7d053f59f542e8154
Yablonskaya Sofya <sofa512@yandex.ru>
2023-10-05 11:58:46 +0300

    task 1

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,25 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decodeFields(encoded, translations) {
+    const decodedList = encoded.map(item => {
+        const decodedItem = {};
+        for (const [key, value] of Object.entries(item)) {
+            if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+                decodedItem[key] = translations[value] || value;
+            } else {
+                decodedItem[key] = value;
+            }
+        }
+        return decodedItem;
+    });
 
+    const uniqueIds = [...new Set(encoded.map(item => item.groupId))];
 
-// console.log(decoded)
+    return { decodedList, uniqueIds };
+}
+
+const { decodedList, uniqueIds } = decodeFields(encoded, translations);
+console.log('Decoded')
+console.log(decodedList);
+console.log(uniqueIds);
